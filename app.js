198ea99e668eacf9e0c99070999b886a4fd1bdbd
Darkzalo <gonza1810@gmail.com>
2020-03-07 15:00:34 -0300

    Added some Security packages

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const helmet = require('helmet');
+const compression = require('compression');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 require('./mongodb')();
 
 
@@ -12,6 +16,10 @@ const home = require('./router/home');
 //middleware
 app.use(express.json());
 app.use(cors());
+app.use(helmet());
+app.use(compression());
+app.use(mongoSanitize());
+app.use(xss());
 
 app.use('/', home);
 app.use('/api/articles/', articles);
